0a653d97801cdfb5fcff1f813dc279eb40de66d7
Huỳnh Thanh Tùng <thanhtungcnttk13@gmail.com>
2017-11-27 09:19:50 +0700

    ckediter_upload_images

---
+++ app/controllers/set_image_clubs_controller.rb
@@ -0,0 +1,41 @@
+class SetImageClubsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :load_club, only:[:show, :update]
+  before_action :set_params_img, only: :update
+
+  def show
+    @list_images = @club.albums.includes(:images)
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def update
+    if @club.update_attributes remote_image_url: "#{@url_upload}"
+      create_acivity @club, Settings.update, @club, current_user
+      flash[:success] = t "club_manager.club.success_update"
+    else
+      flash_error @club
+    end
+    redirect_to organization_club_path(@club.organization.slug, @club)
+  end
+
+  private
+  def load_club
+    @club = Club.find_by slug: params[:id]
+    unless @club
+      flash[:danger] = t "not_found"
+      redirect_to root_url
+    end
+  end
+
+  def set_params_img
+    if params[:club].present?
+      @url_upload = ENV["DOMAIN"] + params[:club][:image]
+    else
+      flash[:danger] = t "select_image"
+      redirect_to organization_club_path(@club.organization.slug, @club)
+    end
+  end
+end
