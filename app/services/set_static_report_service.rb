61a5064fce2fb33760997ccb43fbf3e971dc0631
Huỳnh Thanh Tùng <thanhtungcnttk13@gmail.com>
2018-02-01 11:40:37 +0700

    bao-cao-hoat-don-thanh-vien

--- app/services/set_static_report_service.rb
+++ app/services/set_static_report_service.rb
@@ -8,17 +8,17 @@ class SetStaticReportService
   def load_events
     if @params[:style].to_i == StatisticReport.styles[:monthly]
       if @params[:month].present?
-        @club.events.by_event(@category).by_quarter(@params[:month]).by_years(@params[:date_year])
+        @club.events.by_event(@category).by_months(@params[:month]).by_years(@params[:date_year])
       else
-        @club.events.by_event(@category).by_quarter(Date.current.month).by_years(@params[:date_year])
+        @club.events.by_event(@category).by_months(Date.current.month).by_years(@params[:date_year])
       end
     else
       if @params[:quarter].present?
         quarter = SetQuarterReport.new @params[:quarter]
-        @club.events.by_event(@category).by_quarter(quarter.quarter_report)
+        @club.events.by_event(@category).by_months(quarter.quarter_report)
           .by_years(@params[:date_year])
       else
-        @club.events.by_event(@category).by_quarter(Settings.quarter_1).by_years(@params[:date_year])
+        @club.events.by_event(@category).by_months(Settings.quarter_1).by_years(@params[:date_year])
       end
     end
   end
