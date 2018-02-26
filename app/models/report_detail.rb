7df3f6150b3fecbebec357c1f44b20d522541b62
ThanhTung <thanhtungcnttk13@gmail.com>
2018-02-26 16:45:10 +0700

    edit-report

--- app/models/report_detail.rb
+++ app/models/report_detail.rb
@@ -6,10 +6,10 @@ class ReportDetail < ApplicationRecord
   belongs_to :statistic_report, required: true
   belongs_to :report_category, required: true
 
-  enum style: {pay_money: 1, get_money: 2, other: 3}
+  enum style: {money: 1, active: 2, member: 3, other: 4}
 
   scope :load_detail_budgets, ->{where.not style: :other}
-  scope :load_date_report, ->{where.not date_event: nil}
+  scope :load_date_report, ->{where date_event: nil}
 
   delegate :name, to: :report_category, prefix: :report_category, allow_nil: :true
 end
