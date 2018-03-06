82ba4ef7e31552dc214d7644860610d018169e51
ThanhTung <thanhtungcnttk13@gmail.com>
2018-03-06 10:24:33 +0700

    show-sponsor

--- app/helpers/sponsors_helper.rb
+++ app/helpers/sponsors_helper.rb
@@ -1,5 +1,5 @@
 module SponsorsHelper
-  def check_experience experience
+  def check_experience_detail? experience
     experience[:event].blank? && experience[:time_and_place].blank? &&
       experience[:member].blank? && experience[:communication].blank?
   end
