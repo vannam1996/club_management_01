90a8d167b644f601de3d0812e8cbf543f871b4b5
vannam1996 <vannam051096@gmail.com>
2018-03-15 11:08:22 +0700

    fix-migration

--- db/migrate/20180314081433_create_criterias.rb
+++ db/migrate/20180314081433_create_criterias.rb
@@ -1,8 +1,9 @@
-class CreateCriteria < ActiveRecord::Migration[5.0]
+class CreateCriterias < ActiveRecord::Migration[5.0]
   def change
-    create_table :criteria do |t|
+    create_table :rules do |t|
       t.references :organization, foreign_key: true
       t.text :name
+      t.text :note
 
       t.timestamps
     end
--- db/migrate/20180314081803_create_criteria_details.rb
+++ db/migrate/20180314081803_create_criteria_details.rb
@@ -1,7 +1,7 @@
 class CreateCriteriaDetails < ActiveRecord::Migration[5.0]
   def change
-    create_table :criteria_details do |t|
-      t.references :criteria, foreign_key: true
+    create_table :rule_details do |t|
+      t.references :rule, foreign_key: true
       t.text :content
       t.float :points, default: 0
 
